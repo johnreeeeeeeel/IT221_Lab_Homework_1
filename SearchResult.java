7f56aede6f2d702a50770208c91a3bb708c4e6be
johnreeeeeeeel <malbino.febyjohnrel.onms@gmail.com>
2026-03-06 12:03:48 +0800

    Update SearchResult.java

--- SearchResult.java
+++ SearchResult.java
@@ -1,25 +1,6 @@
 package com.mycompany.it221_lab_homework_1;
 
 public class SearchResult {
-    private boolean found;
-    private int index;
-    private int comparison;
     
-    public SearchResult (boolean found, int index, int comparison) {
-        this.found = found;
-        this.index = index;
-        this.comparison = comparison;
-    }
-    
-    public boolean isFound() {
-        return found;
-    }
-    
-    public int getIndex() {
-        return index;
-    }
-    
-    public int getComparison() {
-        return comparison;
-    }
 }
+
