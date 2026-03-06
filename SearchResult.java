37a6c88435f3163a024369009bbd34a819c1ac4e
johnreeeeeeeel <malbino.febyjohnrel.onms@gmail.com>
2026-03-06 14:25:27 +0800

    Add files via upload

---
+++ SearchResult.java
@@ -0,0 +1,5 @@
+package com.mycompany.it221_lab_homework_1;
+
+public class SearchResult {
+    
+}
---
+++ Searcher.java
@@ -0,0 +1,6 @@
+package com.mycompany.it221_lab_homework_1;
+
+public class Searcher {
+
+    
+}
